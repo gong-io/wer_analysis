5e11c97f89b63ee1a0bf4967a82fe392a1e97d38
omri.allouche@gmail.com <omri.allouche@gmail.com>
2019-12-30 06:36:09 +0200

    added basic code

--- code.py
+++ code.py
@@ -822,9 +822,9 @@ def analyze_wer_folders(folder_truth, folder_hypothesis, folder_output):
 
     filenames = df['filename'].unique()
     df_calls_metadata = get_calls_metadata(filenames)
-    df_calls_metadata['speaker_count_total'] = df_calls_metadata['speaker_count_in_company'] + df_calls_metadata['speaker_count_outside_company'] + df_calls_metadata['speaker_count_company_unknown'])
+    df_calls_metadata['speaker_count_total'] = df_calls_metadata['speaker_count_in_company'] + df_calls_metadata['speaker_count_outside_company'] + df_calls_metadata['speaker_count_company_unknown']
     df_calls_metadata['speaker_count_total'] = df_calls_metadata['speaker_count_total'].fillna(0)
-    
+
     wer_by_filename_with_metadata = pd.merge(left=get_pivot_table_of_edits(df), right=df_calls_metadata, left_on='filename', right_on='call_id')
     save_to_s3( wer_by_filename_with_metadata, s3_filename=folder_output+'/wer_by_filename_with_metadata.csv')
 
