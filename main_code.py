938e685fc5c79d9e1f0180592fbe0f336d5dc754
rotem-1996 <69154004+rotem-1996@users.noreply.github.com>
2021-02-03 15:11:56 +0200

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -341,12 +341,6 @@ def der_metadata(df, folder_output):
     der_by_filename_with_metadata = pd.merge(left=df, right=df_calls_metadata, left_on='filename', right_on='call_id',
                                              how='left')
 
-    #   Add a total line
-    total = der_by_filename_with_metadata.sum(numeric_only=True)
-    total["filename"] = "total"
-    total[["owner_id", "workspace_id", "call_id", "company_id", "topic_model_id"]] = ""
-    der_by_filename_with_metadata = der_by_filename_with_metadata.append(total, ignore_index=True)
-
     save_to_s3(der_by_filename_with_metadata, s3_filename=folder_output + '/der_by_filename_with_metadata.csv')
     der_by_company = der_by_filename_with_metadata.groupby('company_name')['DER'].mean()
     save_to_s3(der_by_company, s3_filename=folder_output + '/der_by_company.csv')
