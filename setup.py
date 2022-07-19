0f1e72047f82a65ad25cb24ac77f566ca711d0ac
rotem-1996 <69154004+rotem-1996@users.noreply.github.com>
2022-07-19 15:33:52 +0300

    make repo into package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wer_analysis',
+    version='0.1',
+    description='Testing installation of Package',
+    url='https://github.com/gong-io/wer_analysis',
+    author='auth',
+    author_email='author@email.com',
+    license='MIT',
+    packages=['wer_analysis'],
+    zip_safe=False)
